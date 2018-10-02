4f4710351722d325de787039f199c774a5c8d475
MPriest <miche.priest@gmail.com>
2018-10-01 19:55:34 -0700

    json parse to get avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -1,22 +1,27 @@
 const request = require("request");
 const secrets = require('./secrets.js');
-console.log("Welcome to the GitHubAvatar Downloader!");
+  console.log("Welcome to the GitHubAvatar Downloader!");
 
-function getRepoContributors(repoOwner, repoName, cb) {
+function getRepoContributors(repoOwner, repoName, handleGetAvatarUrl) {
   var options = {
-  url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
   headers: {
-    "User-Agent": "request"
-    "Authorization": secrets
+    "User-Agent" : "request",
+    "Authorization" : secrets.GITHUB_TOKEN
     }
   };
 
-  request(url, function(err, res, body) {
-    cb(err, body);
+  request(options, function(err, res, body) {
+    let contributors = JSON.parse(res.body);
+    contributors.forEach((contributor) => {
+      handleGetAvatarUrl(contributor.avatar_url)
+    });
   });
- }
+}
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+let getAvatarUrl = function(avatarUrl, ) {
+  console.log(avatarUrl)
+}
+
+
+getRepoContributors("jquery", "jquery", getAvatarUrl);
