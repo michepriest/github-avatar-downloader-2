5d187ba69ba8518d7b27dc5957aeca66cb29f40f
michepriest <miche.priest@gmail.com>
2018-11-07 07:01:25 +0000

    support command line args

---
+++ avatar_image.js
@@ -0,0 +1,5 @@
+const request = require("request");
+
+function downloadImageByUrl(url, filePath) {
+  request.get(url)
+}
--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,10 @@
-const request = require("request");
+const request = require("request");  
 const secrets = require('./secrets.js');
   console.log("Welcome to the GitHubAvatar Downloader!");
+const fs = require("fs");
+const repoOwner = process.argv[2];
+const repoName = process.argv[3]; 
+
 
 function getRepoContributors(repoOwner, repoName, handleGetAvatarUrl) {
   var options = {
@@ -19,9 +23,26 @@ function getRepoContributors(repoOwner, repoName, handleGetAvatarUrl) {
   });
 }
 
-let getAvatarUrl = function(avatarUrl, ) {
-  console.log(avatarUrl)
+// let getAvatarUrl = function(avatarUrl) {
+//   console.log(avatarUrl)
+// }
+
+function downloadImageByURL(url, filePath) {
+  request(url).pipe(fs.createWriteStream(filePath))
 }
 
+getRepoContributors("jquery", "jquery", function(err, results) {
+  console.log("results:", results);
+  for(let i = 0; i < results.length; i++) {
+    if (!repoOwner || !repoName) {
+      console.log("Please enter the repo name and owner.")
+    } else {
+      downloadImageByURL(results[i].avatar_url, './avatars' + results[i].login)
+    }
+  } 
+}) 
+  
 
-getRepoContributors("jquery", "jquery", getAvatarUrl);
+
+
+
